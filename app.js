45689b38babff87f77e46cb7e37613435667814d
Graham MacDonald <graham.macdonald@incentivegmes.com>
2023-03-30 14:22:28 +0100

    A few updates and pull latest PRs

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const { Console } = require("console");
-const { setDefaultResultOrder } = require("dns");
 const GraphQL = require("./graphql");
 const PR = require("./pr");
 
@@ -163,8 +161,7 @@ const createResultsLine = function (result) {
   return output;
 };
 
-
-function getResultsCsv(results){
+function getResultsCsv(results) {
   const header =
     "StartDate,EndDate,TotalOpen,Opened,Closed,AvgChurn,AvgComments,AvgDaysMerged,AvgAgeWhenReviewed,AvgFilesChanged\n";
   const formatted = results.map(createResultsLine);
@@ -177,12 +174,7 @@ function getResultsCsv(results){
   return header + prList;
 }
 
-
 const GenerateGithubData = async () => {
-  // 1: Get al repos
-  // 2: for each repo get a list of PR ids
-  // 3: get each PR
-
   const response = await GraphQL.qraphQlRepositories("incentivegames");
   const repos = response.data.data.organization.repositories.edges
     .filter(hasPR)
@@ -202,12 +194,16 @@ const GenerateGithubData = async () => {
     });
 
     const mergedPrsInRange = pullRequests.filter((item) => {
-      return item.merged && new Date(item.mergedAt) > start && new Date(item.mergedAt) < end
+      return (
+        item.merged &&
+        new Date(item.mergedAt) > start &&
+        new Date(item.mergedAt) < end
+      );
     });
 
     const reviewedPrs = openPrs.filter((item) => {
       return item.reviews.edges.length > 0;
-    })
+    });
 
     const initialValue = 0;
     const totalChurn = openPrs.reduce(
@@ -229,61 +225,120 @@ const GenerateGithubData = async () => {
       initialValue
     );
 
-    const averageAgeWhenReviewed = reviewedPrs.reduce(
-      (accumulator, currentValue) =>
-        accumulator + PR.ageInHoursWhenReviewed(currentValue),
-      initialValue
-    ) / reviewedPrs.length;
+    const averageAgeWhenReviewed =
+      reviewedPrs.reduce(
+        (accumulator, currentValue) =>
+          accumulator + PR.ageInHoursWhenReviewed(currentValue),
+        initialValue
+      ) / reviewedPrs.length;
 
-
-    const averageComments = openPrs.reduce(
-      (accumulator, currentValue) =>
-        accumulator + currentValue.comments.edges.length + currentValue.reviews.edges.length,
-      initialValue
-    ) / openPrs.length;
-
+    const averageComments =
+      openPrs.reduce(
+        (accumulator, currentValue) =>
+          accumulator +
+          currentValue.comments.edges.length +
+          currentValue.reviews.edges.length,
+        initialValue
+      ) / openPrs.length;
 
     let averageDaysMerged = 0;
-    if(mergedPrsInRange.length > 0){
+    if (mergedPrsInRange.length > 0) {
       averageDaysMerged = totalLengthMerged / mergedPrsInRange.length;
     }
 
-    const averageFilesChanged = openPrs.reduce(
-      (accumulator, currentValue) =>
-        accumulator + currentValue.changedFiles,
-      initialValue
-    ) / openPrs.length;
+    const averageFilesChanged =
+      openPrs.reduce(
+        (accumulator, currentValue) => accumulator + currentValue.changedFiles,
+        initialValue
+      ) / openPrs.length;
 
-    const startDateString = start.getFullYear() +    "-" +    addLeadingZeros(start.getMonth() + 1) +    "-" +    addLeadingZeros(start.getDate());
-    const endDateString = end.getFullYear() +    "-" +    addLeadingZeros(end.getMonth() + 1) +    "-" +    addLeadingZeros(end.getDate());
+    const startDateString =
+      start.getFullYear() +
+      "-" +
+      addLeadingZeros(start.getMonth() + 1) +
+      "-" +
+      addLeadingZeros(start.getDate());
+    const endDateString =
+      end.getFullYear() +
+      "-" +
+      addLeadingZeros(end.getMonth() + 1) +
+      "-" +
+      addLeadingZeros(end.getDate());
 
-    allPrs.push({"Start":startDateString, "End":endDateString, "Total":openPrs.length, "Opened":totalOpened, "Closed":totalClosed, "Churn":averageChurn, "AvgComments":averageComments, "AvgDaysMerged":averageDaysMerged, "AvgAgeWhenReviewed":averageAgeWhenReviewed,"AvgFilesChanged":averageFilesChanged});
+    allPrs.push({
+      Start: startDateString,
+      End: endDateString,
+      Total: openPrs.length,
+      Opened: totalOpened,
+      Closed: totalClosed,
+      Churn: averageChurn,
+      AvgComments: averageComments,
+      AvgDaysMerged: averageDaysMerged,
+      AvgAgeWhenReviewed: averageAgeWhenReviewed,
+      AvgFilesChanged: averageFilesChanged,
+    });
 
     start.setDate(start.getDate() + 7);
     end.setDate(end.getDate() + 7);
   }
 
-const timeElapsed = Date.now();
-const today = new Date(timeElapsed);
-const todayString =
-  today.getFullYear() +
-  "-" +
-  addLeadingZeros(today.getMonth() + 1) +
-  "-" +
-  addLeadingZeros(today.getDate());
+  const timeElapsed = Date.now();
+  const today = new Date(timeElapsed);
+  const todayString =
+    today.getFullYear() +
+    "-" +
+    addLeadingZeros(today.getMonth() + 1) +
+    "-" +
+    addLeadingZeros(today.getDate());
 
-let fs = require("fs");
-fs.writeFile(
-  "prdata-" + todayString + ".csv",
-  getResultsCsv(allPrs),
-  function (err) {
-    if (err) return console.log(err);
-    console.log("PR Results > prlist-" + todayString + ".csv");
-  }
-);
+  let fs = require("fs");
+  fs.writeFile(
+    "prdata-" + todayString + ".csv",
+    getResultsCsv(allPrs),
+    function (err) {
+      if (err) return console.log(err);
+      console.log("PR Results > prdata-" + todayString + ".csv");
+    }
+  );
 
-
-
+  //Export stuff
+  start = new Date(2023, 1, 12);
+  end = new Date(start);
+  end.setDate(end.getDate() + 7);
+  //exportDateRange(start, end, pullRequests, todayString);
 };
+
+function exportDateRange(start, end, pullRequests, todayString)
+{
+  const somePrs = pullRequests.filter((item) => {
+    return isOpen(item, start, end);
+  });
+
+  somePrs.sort(function (a, b) {
+    return new Date(a.createdAtDate) - new Date(b.createdAtDate);
+  });
+
+  const stringResult = JSON.stringify(
+    somePrs.map((item) => {
+      return {
+        title: item.title,
+        author: item.author.login,
+        changedFiles: item.changedFiles,
+        createdAtDate: item.createdAtDate,
+        closedAtDate: item.closedAtDate,
+      };
+    })
+  );
+
+  let fs = require("fs");
+  fs.writeFile(
+    "daterange-" + todayString + ".json",
+    stringResult,
+    function (err) {
+      if (err) return console.log(err);
+      console.log("Date Range > daterange-" + todayString + ".json");
+    }
+  );
+}
 
 GenerateGithubData();
--- graphql.js
+++ graphql.js
@@ -5,35 +5,35 @@ const authentication = {
   password: "INSERT",
 };
 
-const graphql_query_pr = 'query($owner:String!,$repo:String!,$prNumber:Int!){repository(owner:$owner,name:$repo){name pullRequest(number:$prNumber){title repository{name}number additions deletions changedFiles createdAt merged mergedAt closed closedAt author{login}timelineItems{totalCount}commits(first:100){edges{node{commit{authoredDate}}}}reviews(first:100){edges{node{createdAt commit{authoredDate}}}}comments(first:100){edges{node{author{login}}}}}}}';
-const graphql_query_repos = 'query($login:String!){organization(login:$login){repositories(first:100){edges{node{name pullRequests(last:1){edges{node{number}}}}}}}}'
+const graphql_query_pr =
+  "query($owner:String!,$repo:String!,$prNumber:Int!){repository(owner:$owner,name:$repo){name pullRequest(number:$prNumber){title repository{name}number additions deletions changedFiles createdAt merged mergedAt closed closedAt author{login}timelineItems{totalCount}commits(first:100){edges{node{commit{authoredDate}}}}reviews(first:100){edges{node{createdAt commit{authoredDate}}}}comments(first:100){edges{node{author{login}}}}}}}";
+const graphql_query_repos =
+  "query($login:String!){organization(login:$login){repositories(first:100){edges{node{name pullRequests(last:1){edges{node{number}}}}}}}}";
 
-async function CallGraphQl(query, variables){
-  const graphQl = {query:query, variables:variables};
+async function CallGraphQl(query, variables) {
+  const graphQl = { query: query, variables: variables };
   const response = await axios({
     method: "POST",
-    url:
-      BASE_URL +
-      `/graphql`,
-    data:graphQl,
+    url: BASE_URL + `/graphql`,
+    data: graphQl,
     auth: authentication,
   });
 
   return response;
 }
 
-async function call_qraphQlRepositories (login){
-  const variables = {login: login};
+async function call_qraphQlRepositories(login) {
+  const variables = { login: login };
   return await CallGraphQl(graphql_query_repos, variables);
 }
 
-
-async function call_qraphQlPullRequest (owner, repo, prNumber){
-  const variables = {owner: owner, repo: repo, prNumber:prNumber};
+async function call_qraphQlPullRequest(owner, repo, prNumber) {
+  const variables = { owner: owner, repo: repo, prNumber: prNumber };
   return await CallGraphQl(graphql_query_pr, variables);
 }
 
 module.exports = {
   qraphQlRepositories: (login) => call_qraphQlRepositories(login),
-  qraphQlPullRequest: (owner, repo, prNumber) => call_qraphQlPullRequest(owner, repo, prNumber),
+  qraphQlPullRequest: (owner, repo, prNumber) =>
+    call_qraphQlPullRequest(owner, repo, prNumber),
 };
